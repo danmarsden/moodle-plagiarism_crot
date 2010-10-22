1843d84850169808268c4f112cf5f5c7fd79705a
Dan Marsden <dan@catalyst.net.nz>
2010-10-22 16:32:07 +1300

    add strings from old 1.9 block

--- lang/en/plagiarism_crot.php
+++ lang/en/plagiarism_crot.php
@@ -28,3 +28,54 @@ $string['newexplain'] = 'For more information on this plugin see: ';
 $string['crot'] = 'Crot';
 $string['usecrot'] ='Enable Crot';
 $string['savedconfigsuccess'] = 'Plagiarism Settings Saved';
+
+//strings copied from block - probably need reworking for new plugin
+$string['block_name'] = 'Anti-Plagiarism';
+$string['course_summary'] = 'Course Summary';
+$string['Topics'] = 'Topics';
+$string['report'] = 'Report';
+$string['settings'] = 'Settings';
+$string['have_to_be_a_teacher'] = '<c>The block is under construction. <br> You have to be a teacher to see its content';
+$string['assignments']='Assignments';
+$string['local']='Local';
+$string['global']='Global';
+$string['settings']='Settings';
+$string['settings_cancelled']='Antiplagiarism settings have been canceled';
+$string['settings_saved']='Antiplagiarism settings have been saved successfully';
+$string['save']='Save';
+$string['select_assignment']='Select the assignment';
+$string['student_name']='Student name';
+$string['similar']='Similar assignments';
+$string['default_threshold']='Default Threshold';
+$string['grammar_size']='Grammar size';
+$string['colours']='Colours';
+$string['window_size']='Window size';
+$string['cluster_distance']='Max Distance between hashes in the text cluster';
+$string['cluster_size']='Minimum cluster size';
+$string['global_search_threshold']='Global search threshold';
+$string['default_threshold']='Defaul Threshold';
+$string['global_search_settings']='Global Search settings';
+$string['global_search_threshold']='Global Search Threshold';
+$string['MS_live_key']='MS Application ID key';
+$string['global_search_query_size']='Global Search Query Size';
+$string['percentage_of_search_queries']='Percentage of search queries for Web search (1-100)';
+$string['number_of_web_documents']='Number of web documents to be downloaded(1=<)';
+$string['clean_tables']='Clean tables (<b>WARNING!</b><br> It removes all Crot data except assignemnts set for check up!<br>Recalculation of fingerprints may cause heavy load on the server)';
+$string['culture_info']='Culture info for global search';
+$string['antiword_path']='Path to antiword<br/>Keep it empty if you do not want to use antiword <br/>to process MS-Word submissions';
+$string['tools']='Tools';
+$string['test_global_serach']='Check it to perform quick test of global search';
+
+
+
+$string['download_inwicast_message'] = '<p>INWICAST Publisher is a very simple and powerful tool designed to record and publish podcasts, videocasts, screencasts and slidecasts for the web and for mobile devices. INWICAST Publisher allows you to:</p><p>
+
+    * record podcasts, videocasts, screencasts and Powerpoint slidescasts<br/>
+    * convert recorded medias to various formats: flv, wmv, mp4, mp3, etc.<br/>
+    * create multimedia content for mobile players like iPod or Zune<br/>
+    * create and manage multimedia playlists<br/>
+    * easily publish your podcasts on your Moodle platform<br/>
+</p>';
+
+$string['FILE_FORMAT_NOT_ALLOWED'] = "Sorry but this file format is not allowed for upload";
+
